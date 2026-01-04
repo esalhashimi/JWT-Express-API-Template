823b1c12072540dad5ed0f815427a57fffc1dc66
esaalhashimi <esahh222@gmail.com>
2026-01-04 10:59:44 +0300

    Finish Template

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 const dotenv = require('dotenv');
+
 dotenv.config();
 const express = require('express');
+
 const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
 const logger = require('morgan');
 
+// Controllers
+const authCtrl = require('./controllers/auth');
 
-//Controllers
-const authCtrl = require("./controllers/auth")
+// Middleware
+const verifyToken = require('./middleware/verify-token');
 
-mongoose.connect(process.env.MONGODB_URL);
-
+mongoose.connect(process.env.MONGODB_URI);
 
 mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
@@ -22,11 +25,15 @@ app.use(express.json());
 app.use(logger('dev'));
 
 // Public Routes
-app.use("/auth" , authCtrl)
+app.use('/auth', authCtrl);
 
+// Protected Routes
+app.use(verifyToken);
 
-//Protected Routes
-
+app.get('/test', (req, res) => {
+  console.log(req.user);
+  res.status(200).json({ message: 'you are logged in!' });
+});
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('The express app is ready!');
